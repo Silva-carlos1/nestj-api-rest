9865f204085033ea59190731a7fb7b79bb8b87d6
Carlos <silva.carloseduardo.ds@gmail.com>
2024-07-04 16:54:39 -0300

    feat: add validation using class-validator for CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,19 @@
+import {
+  MaxLength,
+  IsNotEmpty,
+  IsString,
+  IsPositive,
+  IsNumber,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @MaxLength(225)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsPositive()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsNotEmpty()
   price: number;
 }
