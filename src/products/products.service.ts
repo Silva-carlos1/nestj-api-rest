970a43bbaab10435735f8320ced0749682d84257
Carlos <silva.carloseduardo.ds@gmail.com>
2024-07-04 15:14:28 -0300

    feat(products): implement endpoint to fetch all products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -17,7 +17,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.PrismaService.product.findMany();
   }
 
   findOne(id: number) {
